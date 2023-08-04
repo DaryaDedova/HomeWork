7ae0f9ffd574455d923405b30af7a6ede5a41959
Darya Dedova <dasha_260899@mail.ru>
2023-08-04 12:40:44 +0300

    добавлено ДЗ по JS

---
+++ JS 04.08.2023/script.js
@@ -0,0 +1,6 @@
+alert('04.08.2023');
+
+document.getElementById('fact1').innerHTML = "<p>1. В Ирландии никогда не было кротов.</p>";
+
+document.getElementById('fact2').innerHTML = "<p>3. Скорость распространения лавы после извержения, близка к скорости бега гончей.</p>";
+
